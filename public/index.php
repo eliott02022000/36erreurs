86a3a836b85003458fd41f3924f2381f38855762
Eliott <eliott.jarmoune@outlook.com>
2019-05-16 14:58:25 +0200

    New

--- public/index.php
+++ public/index.php
@@ -31,10 +31,10 @@ if(isset($url_composants[2])){
 
 try{
 
-  list($controller, $action) = ThirtySix\Router::load($action_name,$controller_name);
+  list($controller, $action) = ThirtySix\Router::load($controller_name, $action_name);
 
 }catch(ThirtySix\Exception\ControllerNotFound $e){
-  $controller = new ErrorController();
+  $controller = new Controller\ErrorController();
   $action = "e404";
   $argument = $e;
 
